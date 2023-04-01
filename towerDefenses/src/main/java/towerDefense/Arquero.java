1ed8778922e38562c4dd7b033a5952d10f705b1b
IngSist980 <rmorales20385@ufide.ac.cr>
2023-04-01 12:56:19 -0600

    Tropa
    Definiendo atributos para la clase , contructores y Getters and Setters

--- towerDefenses/src/main/java/towerDefense/Arquero.java
+++ towerDefenses/src/main/java/towerDefense/Arquero.java
@@ -4,6 +4,15 @@ package towerDefense;
  *
  * @author R
  */
-public class Arquero {
+public class Arquero extends Tropa{
+       public Arquero() {
+        super(12, 1);
+    }
+
+    @Override
+    public boolean venceA(Tropa tropa) {
+        return tropa instanceof Mago;
+    }
+    
     
 }
--- towerDefenses/src/main/java/towerDefense/Tropa.java
+++ towerDefenses/src/main/java/towerDefense/Tropa.java
@@ -6,4 +6,32 @@ package towerDefense;
  */
 public class Tropa {
     
+     private int vida;
+    private double danio;
+
+    public Tropa(int vida, double danio) {
+        this.vida = vida;
+        this.danio = danio;
+    }
+
+    public int getVida() {
+        return vida;
+    }
+
+    public void setVida(int vida) {
+        this.vida = vida;
+    }
+
+    public double getDanio() {
+        return danio;
+    }
+
+    public void setDanio(double danio) {
+        this.danio = danio;
+    }
+
+    // Método abstracto para determinar qué tropa vence a qué otra
+    public abstract boolean venceA(Tropa tropa);
+    
+    
 }
