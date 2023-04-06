1c18522de3c3fbbc0f788c1dbf9ab641d9d15c8a
IngSist980 <rmorales20385@ufide.ac.cr>
2023-04-06 09:26:04 -0600

    chore: correción
    Se corrige el error de importar imagen

--- tDprueba3/src/main/GameScreen.java
+++ tDprueba3/src/main/GameScreen.java
@@ -1,30 +1,54 @@
 package main;
+
 import java.awt.Color;
-import javax.swing.JPanel; 
-import java.awt.Graphics; 
+import javax.swing.JPanel;
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.util.Random;
+
 /**
  *
- * @author Ryan Morales. 
+ * @author Ryan Morales.
  */
 public class GameScreen extends JPanel {
-    
-    public GameScreen(){
-        
-        
-        
-    
+
+    private Random random;
+    private BufferedImage img;
+
+    public GameScreen(BufferedImage img) {
+        this.img = img; 
+        random = new Random();
+
     }
-    
-    public void paintComponent(Graphics g){
-         super.paintComponent(g);
-         
-         g.setColor(Color.red);
-         g.fillRect(0, 0, 32, 32);
-         g.fillRect(32, 0, 32, 32);
+
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        g.drawImage(img, 0, 0, null);
+      
+        
+        
+        
+        
+        /* for (int y = 0; y < 20; y++) {
+            for (int x = 0; x < 20; x++) {
+                g.setColor((getRndColor()));
+                  g.fillRect(x *32, y * 32, 32, 32);
+            }
+          
+        }*/
+
         /* g.drawRect(50, 50, 100, 100);
          g.fillRect(50,50, 100, 100);*/
-    
     }
-    
-    
+
+    private Color getRndColor() {
+        int r = random.nextInt(256);
+        int g = random.nextInt(256);
+        int b = random.nextInt(256);
+
+        return new Color(r, g, b);
+
+    }
+
 }
