d4e05cfb9b640161206fca97ade5e6935527a1ad
IngSist980 <rmorales20385@ufide.ac.cr>
2023-04-06 11:42:39 -0600

    Chore: add
    Generando camcbios en la imagen de muestra

--- tDprueba3/src/main/GameScreen.java
+++ tDprueba3/src/main/GameScreen.java
@@ -8,38 +8,42 @@ import java.util.Random;
 
 /**
  *
- * @author Ryan Morales.
+ * author Ryan Morales.
  */
 public class GameScreen extends JPanel {
 
     private Random random;
     private BufferedImage img;
+    private BufferedImage[] sprites = new BufferedImage[100];
 
     public GameScreen(BufferedImage img) {
-        this.img = img; 
-        random = new Random();
+        this.img = img;
 
+        loadSprites();
+
+        random = new Random();
+    }
+
+    private void loadSprites() {
+        for (int y = 0; y < 10; y++) {
+            for (int x = 0; x < 10; x++) {
+                sprites[y * 10 + x] = img.getSubimage(x * 32, y * 32, 32, 32);
+            }
+        }
     }
 
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
 
-        g.drawImage(img, 0, 0, null);
-      
-        
-        
-        
-        
-        /* for (int y = 0; y < 20; y++) {
+        for (int y = 0; y < 20; y++) {
             for (int x = 0; x < 20; x++) {
-                g.setColor((getRndColor()));
-                  g.fillRect(x *32, y * 32, 32, 32);
+                g.drawImage(sprites[getRndInt()], x * 32, y * 32, null);
             }
-          
-        }*/
+        }
+    }
 
-        /* g.drawRect(50, 50, 100, 100);
-         g.fillRect(50,50, 100, 100);*/
+    private int getRndInt() {
+        return random.nextInt(sprites.length);
     }
 
     private Color getRndColor() {
@@ -48,7 +52,5 @@ public class GameScreen extends JPanel {
         int b = random.nextInt(256);
 
         return new Color(r, g, b);
-
     }
-
 }
