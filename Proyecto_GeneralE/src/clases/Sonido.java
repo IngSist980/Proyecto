7ec1233f085efc11a5aa10d65a3a38c462295c44
Luciano0981 <lucianoduran0981@gmail.com>
2023-04-24 21:09:49 -0600

    add class
    Anadiendo clase sonido con sus metodos y modificando clase Main para colocar sonidos

---
+++ Proyecto_GeneralE/src/clases/Sonido.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package clases;
+
+/**
+ *
+ * @author Luis
+ */
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
+import java.io.File;
+import java.io.IOException;
+
+public class Sonido {
+
+    public static void play(String filePath) {
+        new Thread(() -> {
+            try {
+                File soundFile = new File("resources/" + filePath);
+                AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(soundFile);
+                Clip clip = AudioSystem.getClip();
+                clip.open(audioInputStream);
+                clip.start();
+                // Hace que el hilo espere a que termine el sonido antes de cerrarse
+                clip.drain();
+            } catch (UnsupportedAudioFileException | IOException | LineUnavailableException e) {
+                e.printStackTrace();
+            }
+        }).start();
+    }
+}
