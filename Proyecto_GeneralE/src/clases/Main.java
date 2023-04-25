a1902201f01681198be844aacf9cec549da19b6e
Luciano0981 <lucianoduran0981@gmail.com>
2023-04-25 10:17:59 -0600

    add code
    Implementado carpeta de recuerso para los sonidos de inicio  y dos sonidos de victoria y derrota

--- Proyecto_GeneralE/src/clases/Main.java
+++ Proyecto_GeneralE/src/clases/Main.java
@@ -8,7 +8,6 @@ package clases;
  *
  * @author Luis
  */
-
 import clases.*;
 import java.util.Scanner;
 import java.util.concurrent.ThreadLocalRandom;
@@ -19,8 +18,9 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-       Scanner input = new Scanner(System.in);
-Sonido s = new Sonido();
+        Scanner input = new Scanner(System.in);
+        Sonido sonidoInicio = new Sonido();
+        sonidoInicio.play("InicioV2.wav");
         //Creando los Castillos
         Castillo castilloJugador = new Castillo("Castillo Jugador", 10.0, 1);
         Castillo castilloCpu = new Castillo("Castillo CPU", 10.0, 2);
@@ -29,7 +29,6 @@ Sonido s = new Sonido();
         int cont = 1;
 
         System.out.println("***Iniciando juego*** \n\n ");
-        Sonido.play("inicio.wav");
         System.out.println("Castillo de jugador creado con " + castilloJugador.getPuntos() + "puntos. ");
         System.out.println("Castillo de CPU creado con " + castilloJugador.getPuntos() + "puntos. ");
         //Comienza el ciclo del juego
@@ -49,22 +48,22 @@ Sonido s = new Sonido();
             Camino camino1 = new Camino(colaCpuCamino1, colaJugadorCamino1);
             Camino camino2 = new Camino(colaCpuCamino2, colaJugadorCamino2);
 
-            System.out.println("\n\n TURNO/OLEADA " + cont );
-            System.out.println("\nLa cantidad de Tropas a elegir para esta oleada es: " + (cont + 4) );
+            System.out.println("\n\n TURNO/OLEADA " + cont);
+            System.out.println("\nLa cantidad de Tropas a elegir para esta oleada es: " + (cont + 4));
             System.out.println("Las tropas del enemigo para esta oleada son: ");
-            int limiteDeTropasCpu = (int) ((cont+3) * 0.75);
+            int limiteDeTropasCpu = (int) ((cont + 3) * 0.75);
             // Se asignan las tropas del CPU
-            for (int i = 0; i < cont+3; i++) {
+            for (int i = 0; i < cont + 3; i++) {
                 //Se seleccionan random los ID para tropas y caminos
                 int idTropaEnemigo = ThreadLocalRandom.current().nextInt(1, 3 + 1);
                 int idCaminoEnemigo = ThreadLocalRandom.current().nextInt(1, 2 + 1);
                 switch (idTropaEnemigo) {
                     case 1:
-                        if (idCaminoEnemigo == 1 && colaCpuCamino1.getLargo() < limiteDeTropasCpu ) {
+                        if (idCaminoEnemigo == 1 && colaCpuCamino1.getLargo() < limiteDeTropasCpu) {
                             System.out.println("Agregando Mago a camino 1");
                             colaCpuCamino1.insercion(new Nodo(new Tropa("Mago", 1.5, idTropaEnemigo)));
                         } else {
-                            if (colaCpuCamino2.getLargo() < limiteDeTropasCpu ) {
+                            if (colaCpuCamino2.getLargo() < limiteDeTropasCpu) {
                                 System.out.println("Agregando Mago a camino 2");
                                 colaCpuCamino2.insercion(new Nodo(new Tropa("Mago", 1.5, idTropaEnemigo)));
                             } else {
@@ -74,11 +73,11 @@ Sonido s = new Sonido();
                         }
                         break;
                     case 2:
-                        if (idCaminoEnemigo == 1 && colaCpuCamino1.getLargo() < limiteDeTropasCpu ) {
+                        if (idCaminoEnemigo == 1 && colaCpuCamino1.getLargo() < limiteDeTropasCpu) {
                             System.out.println("Agregando Caballero a camino 1");
                             colaCpuCamino1.insercion(new Nodo(new Tropa("Caballero", 2, idTropaEnemigo)));
                         } else {
-                            if (colaCpuCamino2.getLargo() < limiteDeTropasCpu ) {
+                            if (colaCpuCamino2.getLargo() < limiteDeTropasCpu) {
                                 System.out.println("Agregando Caballero a camino 2");
                                 colaCpuCamino2.insercion(new Nodo(new Tropa("Caballero", 2, idTropaEnemigo)));
                             } else {
@@ -88,11 +87,11 @@ Sonido s = new Sonido();
                         }
                         break;
                     case 3:
-                        if (idCaminoEnemigo == 1 && colaCpuCamino1.getLargo() < limiteDeTropasCpu ) {
+                        if (idCaminoEnemigo == 1 && colaCpuCamino1.getLargo() < limiteDeTropasCpu) {
                             System.out.println("Agregando Arquero a camino 1");
                             colaCpuCamino1.insercion(new Nodo(new Tropa("Arquero", 1, idTropaEnemigo)));
                         } else {
-                            if (colaCpuCamino2.getLargo() < limiteDeTropasCpu ) {
+                            if (colaCpuCamino2.getLargo() < limiteDeTropasCpu) {
                                 System.out.println("Agregando Arquero a camino 2");
                                 colaCpuCamino2.insercion(new Nodo(new Tropa("Arquero", 1, idTropaEnemigo)));
                             } else {
@@ -105,14 +104,14 @@ Sonido s = new Sonido();
 
             }
 
-            for (int i = 0; i < cont+4 ; i++) {
+            for (int i = 0; i < cont + 4; i++) {
                 System.out.println("Elija su " + (i + 1) + " tropa: \n 1. Para Mago digite 1 \n 2. Para Caballero digite 2 \n 3. Para Arquero digite 3");
                 int codigoTropa = input.nextInt();
                 System.out.println("Digite en que camino va a ir esta Tropa 1 o 2: ");
                 int codigoCamino = input.nextInt();
                 switch (codigoTropa) {
                     case 1:
-                        if (codigoCamino == 1 ) {
+                        if (codigoCamino == 1) {
                             System.out.println("Agregando Mago a camino 1");
                             colaJugadorCamino1.insercion(new Nodo(new Tropa("Mago", 1.5, codigoTropa)));
                         } else if (codigoCamino == 2) {
@@ -121,7 +120,7 @@ Sonido s = new Sonido();
                         }
                         break;
                     case 2:
-                        if (codigoCamino == 1 ) {
+                        if (codigoCamino == 1) {
                             System.out.println("Agregando Caballero a camino 1");
                             colaJugadorCamino1.insercion(new Nodo(new Tropa("Caballero", 2, codigoTropa)));
                         } else if (codigoCamino == 2) {
@@ -130,14 +129,16 @@ Sonido s = new Sonido();
                         }
                         break;
                     case 3:
-                        if (codigoCamino == 1 ) {
+                        if (codigoCamino == 1) {
                             System.out.println("Agregando Arquero a camino 1");
                             colaJugadorCamino1.insercion(new Nodo(new Tropa("Arquero", 1, codigoTropa)));
                         } else if (codigoCamino == 2) {
                             System.out.println("Agregando Arquero a camino 2");
                             colaJugadorCamino2.insercion(new Nodo(new Tropa("Arquero", 1, codigoTropa)));
                         }
+                        sonidoInicio.stop();
                         break;
+                        
                 }
             }
             System.out.println("\nEnfrentando tropas del camino 1: ");
@@ -145,12 +146,12 @@ Sonido s = new Sonido();
             System.out.println("\nEnfrentando tropas del camino 2: ");
             ResultadoEnfrentamiento resultadoEnfrentamiento2 = camino2.enfrentarColas();
 
-            if (resultadoEnfrentamiento1.getIdCastillo() == 1){
+            if (resultadoEnfrentamiento1.getIdCastillo() == 1) {
                 castilloJugador.setPuntos(castilloJugador.getPuntos() - resultadoEnfrentamiento1.getDanno());
             } else {
                 castilloCpu.setPuntos(castilloCpu.getPuntos() - resultadoEnfrentamiento1.getDanno());
             }
-            if (resultadoEnfrentamiento2.getIdCastillo() == 1){
+            if (resultadoEnfrentamiento2.getIdCastillo() == 1) {
                 castilloJugador.setPuntos(castilloJugador.getPuntos() - resultadoEnfrentamiento2.getDanno());
             } else {
                 castilloCpu.setPuntos(castilloCpu.getPuntos() - resultadoEnfrentamiento2.getDanno());
@@ -160,12 +161,28 @@ Sonido s = new Sonido();
         } while (castilloCpu.getPuntos() > 0 && castilloJugador.getPuntos() > 0);
 
         if (castilloJugador.getPuntos() > 0 && castilloCpu.getPuntos() <= 0) {
+            sonidoInicio.stop();
+            Sonido sonidoVictoria = new Sonido();
+            sonidoVictoria.play("Victoria.wav");
             System.out.println("\nEl jugador es el ganador");
         } else if (castilloJugador.getPuntos() <= 0 && castilloCpu.getPuntos() > 0) {
+            sonidoInicio.stop();
+            Sonido sonidoDerrota = new Sonido();
+            sonidoDerrota.play("Perdiste.wav");
             System.out.println("\nEl CPU es el ganador");
         } else if (castilloJugador.getPuntos() <= 0 && castilloCpu.getPuntos() <= 0) {
+            sonidoInicio.stop();
+            // Puedes agregar un sonido de empate aquí si lo deseas
             System.out.println("\nEl resultado es EMPATE");
         }
+
+// Agrega un breve retardo para permitir que el sonido se reproduzca antes de que el programa termine.
+        try {
+            Thread.sleep(5000); // espera 5 segundos
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
     }
 
 }
